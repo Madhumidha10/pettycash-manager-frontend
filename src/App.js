c13ee3f681ad332edcbe898ff8221803e0f82bbd
Madhumidha10 <madhu4achieve@gmail.com>
2022-05-25 18:02:59 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Forgot from './Pages/Forgot';
+import Login from './Pages/Login';
+import SignUp from './Pages/SignUp';
+import {Routes,Route} from 'react-router-dom'
+import Dashboard from './Pages/Dashboard';
+import ChangePWD from './Pages/ChangePWD';
+import AddCharts from './components/AddCharts';
+import PageNotFound from './Pages/PageNotFound';
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+       <Route exact path='/' element={<Login />} />
+         <Route path='/forgot' element={<Forgot />} />
+        <Route path='/register' element={<SignUp />} />
+        <Route path="/api/password-reset/:userid/:token" element={<ChangePWD />} />
+        
+        
+        {/* dashboard routes */}
+        <Route path="/dashboard" element={localStorage.getItem("token")?<Dashboard />:<Login />} />
+        <Route path="/dashboard/company" element={localStorage.getItem("token")?<Dashboard />:<Login />} />
+        <Route path="/dashboard/addin" element={localStorage.getItem("token")?<Dashboard />:<Login />} />
+        <Route path="/dashboard/addout" element={localStorage.getItem("token")?<Dashboard />:<Login />} />
+        <Route path="/dashboard/openbalance" element={localStorage.getItem("token")?<Dashboard />:<Login />} />
+        <Route path="/dashboard/reports" element={localStorage.getItem("token")?<Dashboard />:<Login />} />
+        <Route path="/dashboard/expenselist" element={localStorage.getItem("token")?<Dashboard />:<Login />} />
+        <Route path="/dashboard/incomelist" element={localStorage.getItem("token")?<Dashboard />:<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+
     </div>
   );
 }
